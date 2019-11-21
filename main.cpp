70feab93e85bbf350151d634e84563ebc445d46d
Shane McDonough <shanemcdman@gmail.com>
2019-11-20 18:54:17 -0800

    added a bunch of comments

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 #include<GL/glut.h>
 
-struct coord{
+struct coord{//literally the simplest struct.
 	double x;
 	double y;
 };
 
-double zoomnum = 1;
-char direction[4] = {'r','d','l','u'};
+double zoomnum = 1;//how zoomed in it is. ya know.
+char direction[4] = {'r','d','l','u'};//uses counter to tell direction
 
 void curve(coord point1, coord point2)
 {
+	glLineWidth(2.5f);//bigger line width
+
+	//will be a curve rn its a line lmao.
 	glBegin(GL_LINES);
 		glVertex2d(point1.x, point1.y);	
 		glVertex2d(point2.x, point2.y);	
 	glEnd();
+
+	glLineWidth(1.0f);//line width default
 }//curve
 
 void HolQuad(coord point1, coord point2, coord point3, coord point4, int num)//print 4 lines in a quadralateral corrioding to the 4 points
@@ -23,30 +28,31 @@ void HolQuad(coord point1, coord point2, coord point3, coord point4, int num)//p
 	//change color
 	if(direction[num%4] == 'r')
 	{
-		glColor3f(1.0f, 0.0f, 0.0f);//color 1
+		glColor3f(1.0f, 0.0f, 0.0f);//color 1 red
 	}//if r
 	else if(direction[num%4] == 'd')
 	{
-		glColor3f(0.0f, 1.0f, 0.0f);//color 2
+		glColor3f(0.0f, 1.0f, 0.0f);//color 2 green
 	}//if d
 	else if(direction[num%4] == 'l')
 	{
-		glColor3f(0.0f, 0.0f, 1.0f);//color 3
+		glColor3f(0.0f, 0.0f, 1.0f);//color 3 blue
 	}//if l
 	else if(direction[num%4] == 'u')
 	{
-		glColor3f(0.0f, 0.0f, 0.0f);//color 4
+		glColor3f(0.0f, 0.0f, 0.0f);//color 4 black
 	}//if u
 
 
-	glBegin(GL_QUADS);
+	glBegin(GL_QUADS);//print colored quad
 		glVertex2d(point1.x, point1.y);
 		glVertex2d(point2.x, point2.y);
 		glVertex2d(point3.x, point3.y);
 		glVertex2d(point4.x, point4.y);
 	glEnd();
-	glColor3f(1.0f, 1.0f, 1.0f);
-	
+	glColor3f(1.0f, 1.0f, 1.0f);//white	
+
+	//print hollow quad
 	glBegin(GL_LINES);
 		glVertex2d(point1.x, point1.y);
 		glVertex2d(point2.x, point2.y);
@@ -78,8 +84,9 @@ return false;
 void DrawFib()
 {
 	double minlength = 0.0005 / zoomnum;//side length of first triangle
-	int counter = 0;
-	//starting square
+	int counter = 0;//used to determine direction
+
+	//starting square initial points
 	coord point[4];
 	point[0].x = minlength;
 	point[0].y = minlength;
@@ -93,15 +100,16 @@ void DrawFib()
 	//starting sequence
 	double prevlength[3] = {0, minlength * 2, 0};
 	
-	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );//I do as the sample code says
 	while(under1(point))
 	{
 		HolQuad(point[0], point[1], point[2], point[3], counter);
 		
-		//change position and size of square
+		//change position and size of square and also print curve
 		if(direction[counter%4] == 'r')
 		{
 			curve(point[2], point[0]);
+
 			for(int i = 0; i < 4; i++)
 			{
 				
@@ -115,6 +123,7 @@ void DrawFib()
 		else if(direction[counter%4] == 'd')
 		{
 			curve(point[1], point[3]);
+
 			for(int i = 0; i < 4; i++)
 			{
 				point[i].y -= prevlength[1];
@@ -127,6 +136,7 @@ void DrawFib()
 		else if(direction[counter%4] == 'l')
 		{
 			curve(point[2], point[0]);
+
 			for(int i = 0; i < 4; i++)
 			{
 				point[i].x -= prevlength[1];
@@ -139,6 +149,7 @@ void DrawFib()
 		else if(direction[counter%4] == 'u')
 		{
 			curve(point[1], point[3]);
+
 			for(int i = 0; i < 4; i++)
 			{
 				point[i].y += prevlength[1];
@@ -149,19 +160,23 @@ void DrawFib()
 			point[3].x += prevlength[0];
 		}//if u
 
-		prevlength[2] = prevlength[0] + prevlength[1]; //set length of squares
+		//set length of squares
+		prevlength[2] = prevlength[0] + prevlength[1];
 		prevlength[0] = prevlength[1];
 		prevlength[1] = prevlength[2];
-		counter++;
+		counter++;//increment
 	}//while(under1)
 
-	zoomnum *= 0.999;
-	if(zoomnum <= 0.14705882352)//one full loop around the square
+	zoomnum *= 0.999;//zoom in
+	if(zoomnum <= 0.14705882352)//if one square has become the same size as the next biggest of the same color
 	{
-		zoomnum = 1;
+		zoomnum = 1;// reset scale
 	}//if
+	//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+	//This is done so instead of infinitely increasing the size of a set number of squares, it increases the
+	//size to until it is smaller than that big decimal ((5/34) => square/next biggest of same color)
 
-	glutSwapBuffers();
+	glutSwapBuffers();//I do as sample code commands
 }//DrawFib
 
 
