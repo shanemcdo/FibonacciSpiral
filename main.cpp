5977f914f72af8f67052a02186e3040ad2313a9f
Shane McDonough <shanemcdman@gmail.com>
2019-11-21 14:45:19 -0800

    finshed curve function. next step translate

--- main.cpp
+++ main.cpp
@@ -3,190 +3,36 @@
 #include<math.h>
 
 double zoomnum = 1;//how zoomed in it is. ya know.
-char direction[4] = {'r','d','l','u'};//uses counter to tell direction
 //TODO: replace the counter inputs in the functions with just a char.
 // - Make direction a local variable
 // - make curve in a seperate file.
+
 
 struct coord{//literally the simplest struct.
 	double x;
 	double y;
 };
 
-void cleanarr(coord a[], int count, int iter, double r)
-{
-		if(direction[count % 4] == 'd')
-		{
-			a[iter].x -= abs(r);	
-		}//if d
-		else if(direction[count % 4] == 'l')
-		{
-			a[iter].x -= abs(r);
-			a[iter].y += abs(r);
-		}//if l
-		else if(direction[count % 4] == 'u')
-		{
-			a[iter].y += abs(r);
-		}//if u
-}//cleanarr
+void curve(coord point1, coord point2, char dir);
 
-void curve(coord point1, coord point2, int count)
-{
-	glLineWidth(2.5f);//bigger line width
-	
-	//PRINT CURVE
-	double r = abs(point1.y - point2.y);//find radius
-
-	std::cout << "Radius: " << r << std::endl << std::endl;
-
-	coord c;
-	coord arr[11];
-
-	c.x = abs(point1.x);
-	for(int i = 0; i < 11;i++)
-	{
-		c.y = sqrt(pow(r,2) - pow(c.x,2));	
-
-		if(direction[count % 4] == 'r')
-		{
-			c.x = abs(c.x) * -1;
-			c.y = abs(c.y);
-		}//if r
-		else if(direction[count % 4] == 'd')
-		{
-
-			c.x = abs(c.x);
-			c.y = abs(c.y);
-			r = (abs(r) * -1);
-		}//if d
-		else if(direction[count % 4] == 'l')
-		{
-			c.x = abs(c.x);
-			c.y = abs(c.y) * -1;
-			r = (abs(r) * -1);
-		}//if l
-		else if(direction[count % 4] == 'u')
-		{
-			c.x = abs(c.x) * -1;
-			c.y = abs(c.y) * -1;
-		}//if u
-
-
-		arr[i].x = c.x;
-		arr[i].y = c.y;
-		cleanarr(arr, count, i ,r);
-		std::cout << "point " << i << ": " << c.x << " " << c.y << std::endl;
-		c.x += r/10;
-	}//for c.x
-	for(int i = 1; i < 10; i++)
-	{
-		glBegin(GL_LINES);
-			glVertex2d(arr[i].x, arr[i].y);
-			glVertex2d(arr[i + 1].x, arr[i + 1].y);
-		glEnd();
-	}//for i
-
-	//SECOND HALF
-	r = abs(point1.y - point2.y);//find radius
-
-	c.y = abs(point1.y);
-	for(int i = 0; i < 10;i++)
-	{
-		c.x = (sqrt(pow(r,2) - pow(c.y,2)) * -1);	
-
-		if(direction[count % 4] == 'r')
-		{
-			c.x = abs(c.x) * -1;
-			c.y = abs(c.y);
-		}//if r
-		else if(direction[count % 4] == 'd')
-		{
-			c.x = abs(c.x);
-			c.y = abs(c.y);
-		}//if d
-		else if(direction[count % 4] == 'l')
-		{
-			c.x = abs(c.x);
-			c.y = abs(c.y) * -1;
-			r = abs(r) * -1;
-		}//if l
-		else if(direction[count % 4] == 'u')
-		{
-			c.x = abs(c.x) * -1;
-			c.y = abs(c.y) * -1;
-			r = abs(r) * -1;
-		}//if u
-
-		arr[i].x = c.x;
-		arr[i].y = c.y;
-		cleanarr(arr, count, i, r);
-		std::cout << "point " << i << ": " << c.x << " " << c.y << std::endl;
-		c.y += r/10;
-	}//for c.y
-
-	for(int i = 0; i < 9; i++)
-	{
-		glBegin(GL_LINES);
-			glVertex2d(arr[i].x, arr[i].y);
-			glVertex2d(arr[i + 1].x, arr[i + 1].y);
-		glEnd();
-	}//for i
-
-
-
-	
-
-
-	glLineWidth(1.0f);//line width default
-}//curve
-
-void temp()
-{
-	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );//I do as the sample code says
-
-	coord point[3];
-	point[0].x = -0.5;
-	point[0].y = 0;
-	point[1].x = 0;
-	point[1].y = 0.5;
-
-	curve(point[0], point[1], 0);
-	curve(point[0], point[1], 1);
-	curve(point[0], point[1], 2);
-	curve(point[0], point[1], 3);
-	
-	glBegin(GL_LINES);
-		glVertex2d(0,1);
-		glVertex2d(0,-1);
-	glEnd();
-	glBegin(GL_LINES);
-		glVertex2d(1,0);
-		glVertex2d(-1,0);
-	glEnd();
-
-
-
-	glutSwapBuffers();//I do as sample code commands
-}//temp
-
-void HolQuad(coord point1, coord point2, coord point3, coord point4, int num)//print 4 lines in a quadralateral corrioding to the 4 points
+void HolQuad(coord point1, coord point2, coord point3, coord point4, char dir)//print 4 lines in a quadralateral corrioding to the 4 points
 {
 	
 
 	//change color
-	if(direction[num%4] == 'r')
+	if(dir == 'r')
 	{
 		glColor3f(1.0f, 0.0f, 0.0f);//color 1 red
 	}//if r
-	else if(direction[num%4] == 'd')
+	else if(dir == 'd')
 	{
 		glColor3f(0.0f, 1.0f, 0.0f);//color 2 green
 	}//if d
-	else if(direction[num%4] == 'l')
+	else if(dir == 'l')
 	{
 		glColor3f(0.0f, 0.0f, 1.0f);//color 3 blue
 	}//if l
-	else if(direction[num%4] == 'u')
+	else if(dir == 'u')
 	{
 		glColor3f(0.0f, 0.0f, 0.0f);//color 4 black
 	}//if u
@@ -233,6 +79,7 @@ void DrawFib()
 {
 	double minlength = 0.0005 / zoomnum;//side length of first triangle
 	int counter = 0;//used to determine direction
+	char direction[4] = {'r','d','l','u'};//uses counter to tell direction
 
 	//starting square initial points
 	coord point[4];
@@ -251,8 +98,8 @@ void DrawFib()
 	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );//I do as the sample code says
 	while(under1(point))
 	{
-		HolQuad(point[0], point[1], point[2], point[3], counter);
-		//curve(point[2], point[0], counter);
+		HolQuad(point[0], point[1], point[2], point[3], direction[counter % 4]);
+		curve(point[1], point[3], direction[counter % 4]);
 		
 		//change position and size of square and also print curve
 		if(direction[counter%4] == 'r')
@@ -332,10 +179,6 @@ int main(int argc, char *argv[])
 	//functions
 	glutDisplayFunc(DrawFib);
 	glutIdleFunc(DrawFib);
-	
-	//glutDisplayFunc(temp);
-	//glutIdleFunc(temp);
-
 
 	//loop
 	glutMainLoop();
--- makefile
+++ makefile
@@ -1,2 +1,8 @@
-all:
-	g++ main.cpp -lopenGL32 -lfreeGLUT -o test
+all: main.o curve.o
+	g++ main.o curve.o -lopenGL32 -lfreeGLUT -o test
+main.o: main.cpp
+	g++ main.cpp -c -o main.o -lopenGL32 -lfreeGLUT 
+curve.o: curve.cpp
+	g++ curve.cpp -lopenGL32 -lfreeGLUT -c -o curve.o
+c:
+	g++ curve.cpp -o curve.exe -lfreeGLUT -lopenGL32
